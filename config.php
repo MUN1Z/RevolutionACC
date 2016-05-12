f0d89f39eb491e69c94d68c65c01d0bf565cb6a0
Felipe Muniz <mrmunz2012@gmail.com>
2016-05-12 00:04:24 -0300

    Realizado o SetUp do projeto, estruturado as pastas e criado o config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MUNIZ
+ * Date: 11/05/2016
+ * Time: 23:53
+ * Project: RevolutionACC
+ */
+
+//Constantes da conexão com o banco de dados
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DBSA', 'ps');
