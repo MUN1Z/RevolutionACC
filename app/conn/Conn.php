8ecb0267bf7e5e9d7e8b1e44d98929067625267e
Felipe Muniz <mrmunz2012@gmail.com>
2016-05-12 00:28:37 -0300

    Criada a classe Conn responsável pela conexão com o banco de dados usando PDO

---
+++ app/conn/Conn.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MUNIZ
+ * Date: 12/05/2016
+ * Time: 00:22
+ * Description: Classe abstrata de conexão Padrão singleton, retorna um objeto PDO pelo método estatico getCon
+ */
+
+namespace conn;
+
+
+class Conn{
+
+    private static $Host = HOST;
+    private static $User = USER;
+    private static $Pass = PASS;
+    private static $Dbsa = DBSA;
+
+    /** @var PDO */
+    private static $Connect = null;
+
+    /**
+     * Conecta com o banco de dados com o pattern singleton.
+     * Retorna um objeto PDO!
+     */
+    private static function Conectar() {
+        try {
+            if (self::$Connect == null):
+                $dsn = 'mysql:host=' . self::$Host . ';dbname=' . self::$Dbsa;
+                $options = [ PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES latin1'];
+                self::$Connect = new PDO($dsn, self::$User, self::$Pass, $options);
+            endif;
+        } catch (PDOException $e) {
+            PHPErro($e->getCode(), $e->getMessage(), $e->getFile(), $e->getLine());
+            die;
+        }
+
+        self::$Connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return self::$Connect;
+    }
+
+    /** Retorna um objeto PDO Singleton Pattern. */
+    public static function getConn() {
+        return self::Conectar();
+    }
+
+}
